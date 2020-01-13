6c00e6b3304ba815626ef39b2938475f3c59b6a0
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-13 14:19:29 -0600

    Update prueba1.py

--- MonTemp/prueba1.py
+++ MonTemp/prueba1.py
@@ -1,9 +1,9 @@
 import os
 os.system('which python3')
 
-from prueba1_ui import *
-from info_ui import *
-from segunda_ui import *
+from MonTemp.prueba1_ui import *
+from MonTemp.info_ui import *
+from MonTemp.segunda_ui import *
 from pyqtgraph import QtGui, QtCore
 from PyQt5.QtWidgets import QDialog,QMessageBox,QLabel
 import pyqtgraph as pg
