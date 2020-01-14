a815361b410b9e0665bfee420264cf56abdbdbd4
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-14 16:11:35 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -4,5 +4,7 @@ try:
   os.system('pip3 install setuptools')
   os.system('pip3 install -r requeriments.txt')
   os.system('python3 setup.py install')
+  os.system('exit')
+  os.system('nano ~/.bashrc')
 except:
   print('Error de instalación')
