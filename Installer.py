d5e5cc9648cbdf575172829443d1e622ae1d1492
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-14 17:45:09 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -7,7 +7,7 @@ try:
   os.system('python3 setup.py install')
   path="pyhon3 "+subprocess.getoutput("pwd")+"/MonTemp/prueba1.py"
   os.system('pwd')
-  os.system("echo " + patch + ">> run")
+  #os.system("echo " + patch + ">> run")
   
 except:
   print('Error de instalación')
