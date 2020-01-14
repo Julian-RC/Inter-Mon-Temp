74943a0407553408e44e9090317a52d0edd5c08b
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-14 16:55:26 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,8 +1,15 @@
 import os
+import subprocess
 try:
   os.system('apt-get install python3-pip')
   os.system('pip3 install setuptools')
   os.system('pip3 install -r requeriments.txt')
   os.system('python3 setup.py install')
+  path=subprocess.getoutput("pwd")
+  os.system('cd MonTemp')
+  f = open ("prueba1.py", "a")
+  f.write("os.system('cd'+path)")
+  f.close()
+  
 except:
   print('Error de instalación')
