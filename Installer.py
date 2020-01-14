bf2fdeac7347e940625eb1cb9ffcacb586a7068a
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-14 13:22:06 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,5 +1,7 @@
 import os
 try:
+  os.system('apt-get install python3-pip')
+  os.system('apt-get install setuptools')
   os.system('pip3 install -r requeriments.txt')
   os.system('python3 setup.py install')
 except:
