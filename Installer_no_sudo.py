975697995e2fb730b66d22ee6e66ab2aade8e45d
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-15 13:15:14 -0600

    Update Installer_no_sudo.py

--- Installer_no_sudo.py
+++ Installer_no_sudo.py
@@ -3,9 +3,9 @@ import subprocess
   
 path = subprocess.getoutput("pwd")+"/MonTemp/prueba1.py"
 os.system("mkdir -p ~/.myPrograms")
-a = "cd ~/.myPrograms && ln -s "+ path + "   InterMonTemp_3"
+a = "cd ~/.myPrograms && ln -s "+ path + "   InterMonTemp_4"
 os.system(a)
 print(2)
 os.system("echo 'export PATH=$PATH:~/.myPrograms' >> ~/.bashrc")
 print(3)
-os.system("cd MonTemp && chmod u+x prueba1.py")
+#os.system("cd MonTemp && chmod u+x prueba1.py")
