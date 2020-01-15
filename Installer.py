8e73ed91f3478ac5f947c47d54b4c6040c70d6e8
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-15 13:01:44 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -3,22 +3,6 @@ import subprocess
 
 try:
   os.system('apt-get install python3-pip')
-  #os.system('pip3 install setuptools')
   os.system('pip3 install -r requeriments.txt')
-  #os.system('python3 setup.py install')
-  path = subprocess.getoutput("pwd")+"/MonTemp/prueba1.py"
-  print(path)
-  #print(type(path))
-  #os.system('pwd')
-  #os.system("echo '" + path + "'+>> run.py")
-  os.system("mkdir -p ~/.myPrograms")
-  print(1)
-  a = "cd ~/.myPrograms && ln -s "+ path + "   InterMonTemp_2"
-  print(a)
-  os.system(a)
-  print(2)
-  os.system("echo 'export PATH=$PATH:~/.myPrograms' >> ~/.bashrc")
-  print(3)
-  os.system("cd MonTemp && chmod u+x prueba1.py")
 except:
-  print('Error de instalación')
+  print('Error de instalación de Paquetes')
