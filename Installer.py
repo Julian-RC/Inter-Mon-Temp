5ca0106074e827b4aa76aff0747f2505f9abfc1d
Julian-RC <57238552+Julian-RC@users.noreply.github.com>
2020-01-15 16:15:11 -0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -3,5 +3,6 @@ try:
   os.system('apt-get install python3-pip')
   os.system('pip3 install -r requeriments.txt')
   os.system('python3 setup.py install')
+  
 except:
   print('Error de instalación de Paquetes')
