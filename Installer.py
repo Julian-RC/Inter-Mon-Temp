6c2cd9edd90ab19e3c94d9c10d2814dfd6df7842
rakshasa <rakshasa@osito.rakshasa>
2020-01-18 22:07:02 -0600

    config-file

--- Installer.py
+++ Installer.py
@@ -1,8 +1,10 @@
 import os
-try:
-  os.system('apt upgrade python3-pip')
-  os.system('pip3 install -r requeriments.txt')
-  os.system('python3 setup.py install')
+import subprocess
+#try:
+#  os.system('apt upgrade python3-pip')
+#  os.system('pip3 install -r requeriments.txt')
+#  os.system('python3 setup.py install')
+os.system('cd MonTemp && cp config_file.txt config_file2.txt '+ subprocess.getoutput(' whereis ls'))
   
-except:
-  print('Error de instalación de Paquetes')
+#except:
+ # print('Error de instalación de Paquetes')
