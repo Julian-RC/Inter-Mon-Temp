feb0a6cc4e2c9851dcc1d69deba0136c334b1fda
rakshasa <rakshasa@osito.rakshasa>
2020-01-21 09:34:08 -0600

    text

--- MonTemp/prueba1.py
+++ MonTemp/prueba1.py
@@ -200,6 +200,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.lastdata.setEnabled(True)
         label_scroll+='                              Acquisition has begun\n'
         label_scroll+='-------------------------------------------------------------------------\n'
+        self.scrollArea.setWidget(QtWidgets.QLabel(label_scroll))
+        self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
         while Start:
             try:
                     DataTemp.GetData()
@@ -305,6 +307,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         global label_scroll,filename,filename2,patch
         patch = QtWidgets.QFileDialog.getExistingDirectory(self, 'Buscar Carpeta', QtCore.QDir.homePath())
         if patch:
+            label_scroll+='                               Wait a moment Please\n'
+            label_scroll+='-------------------------------------------------------------------------\n'
+            self.scrollArea.setWidget(QtWidgets.QLabel(label_scroll))
+            self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
+        if patch:
             label_scroll+='                                  Selected folder\n'
             label_scroll+='-------------------------------------------------------------------------\n'
             self.scrollArea.setWidget(QtWidgets.QLabel(label_scroll))
@@ -329,12 +336,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.pushButton.setEnabled(True)
                 self.Todos.setEnabled(True)
                 self.radioButton.setEnabled(True)
-                label_scroll+='Push "start" for begin adquisition\n'
+                label_scroll+='                      Push "start" for begin adquisition\n'
                 label_scroll+='-------------------------------------------------------------------------\n'
                 self.scrollArea.setWidget(QtWidgets.QLabel(label_scroll))
                 self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
             except:
-                label_scroll += 'Error al cargar la configuración de los modulos\n'
+                label_scroll += '                 Error al cargar la configuración de los modulos\n'
                 label_scroll+='-------------------------------------------------------------------------\n'
                 self.scrollArea.setWidget(QtWidgets.QLabel(label_scroll))
                 self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().maximum())
