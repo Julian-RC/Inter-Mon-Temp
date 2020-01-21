2e8375e874de60e2af1dc751c096157986d45418
rakshasa <rakshasa@osito.rakshasa>
2020-01-21 15:09:15 -0600

    plot-refresh

--- MonTemp/prueba1.py
+++ MonTemp/prueba1.py
@@ -550,7 +550,7 @@ class LivePlotter(object):
 
 
 	def add(self, x):
-			for i in range(Time_curva1)
+			for i in range(Time_curva1):
 					if Time_curva1[0][1]-Time_curva1[0][-1]>100:
 						np.delete(self.Data_curva1, 0)
 						np.delete(self.Data_curva2, 0)
